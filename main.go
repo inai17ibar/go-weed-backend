b7c78c482a5726c68e8804468652902be7bb24f8
Soichiro Inatani <inai17ibar@gmail.com>
2023-08-20 16:31:23 +0900

    Add get json data func.
    ref: https://zenn.dev/tamanegi/articles/ff8679aabd2994

--- main.go
+++ main.go
@@ -1,6 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
@@ -12,5 +33,6 @@ func setupRouter() *gin.Engine {
 
 func main() {
 	r := setupRouter()
+	r.GET("/albums", getAlbums)
 	r.Run(":8080")
 }
