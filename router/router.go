9042bcd5dbfe6ac4ace5adeec3c1978a0905af6b
Soichiro Inatani <inai17ibar@gmail.com>
2023-10-06 07:14:37 +0900

    ハードコーディングの修正。データべーファイルの追跡削除。

--- router/router.go
+++ router/router.go
@@ -1,19 +1,63 @@
 package router
 
 import (
+	"encoding/json"
 	"go-weed-backend/internal/handler"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/handlers"
 )
 
+type Config struct {
+	ServerPort string     `json:"ServerPort"`
+	CORS       CORSConfig `json:"CORS"`
+}
+
+type CORSConfig struct {
+	AllowedOrigins []string `json:"AllowedOrigins"`
+	AllowedMethods []string `json:"AllowedMethods"`
+	AllowedHeaders []string `json:"AllowedHeaders"`
+}
+
+func LoadConfig(filePath string) (*Config, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	config := &Config{}
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
 func NewRouter() http.Handler {
 	mux := http.NewServeMux()
 
+	env := os.Getenv("APP_ENV")
+	var configFilePath string
+	if env == "production" {
+		configFilePath = "config.production.json"
+	} else {
+		configFilePath = "config.local.json"
+	}
+
+	config, err := LoadConfig(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %s", err)
+	}
+
 	corsHandler := handlers.CORS(
-		handlers.AllowedOrigins([]string{"http://localhost:3000"}),
-		handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS", "DELETE", "PUT"}),
-		handlers.AllowedHeaders([]string{"Content-Type"}),
+		handlers.AllowedOrigins(config.CORS.AllowedOrigins),
+		handlers.AllowedMethods(config.CORS.AllowedMethods),
+		handlers.AllowedHeaders(config.CORS.AllowedHeaders),
 	)
 
 	mux.Handle("/todos", corsHandler(http.HandlerFunc(handler.GetTodos)))
